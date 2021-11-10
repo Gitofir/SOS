2c6879aadb2e144fa49235de10e39c50af422e80
SapirTar <sapirtt43@gmail.com>
2021-11-11 00:15:44 +0200

    connect to twitter api, display not working

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
+using TweetSharp;
 
 namespace WebApp.Controllers
 {
@@ -44,5 +45,26 @@ namespace WebApp.Controllers
         {
             return View();
         }
+
+        ///* TWITTER API */
+        ///*------------------------------------------------*/
+
+        //public static string _ConsumerKey = "79MxgKLaBEplt7HiQzkj3oqTp";
+        //public static string _ConsumerSecret = "7nAfcs3IWSf8zbZhP7f5D61RYIhvPljg9bhwyIPQUFhZlYp2nD";
+        //public static string _AccessToken = "1458372727140671495-s0nFlhJoqJ6MGYoQPxC2p7YZ1PF0fG";
+        //public static string _AccessTokenSecret = "YguTTU40A6KXoWfH0AHDY7k4gXItPM8po5ANPeymCIy2i";
+
+        //protected TwitterSearchResult GetTwitterSearchResult()
+        //{
+
+        //    var tweets_search = new SearchOptions { Q = "#stocks", Resulttype = TwitterSearchResultType.Popular };
+        //    TwitterService twService = new TwitterService(_ConsumerKey, _ConsumerSecret, _AccessToken, _AccessTokenSecret);
+        //    TwitterSearchResult searchResult = twService.Search(tweets_search);
+        //    return searchResult;
+        //}
+
+       
     }
 }
+
+
