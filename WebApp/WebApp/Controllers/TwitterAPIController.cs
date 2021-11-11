820fe6f6c5302380a0f9bc30e50875b22c7ad1e0
Ofir Shen <ofirshen@gmail.com>
2021-11-11 13:51:30 +0200

    working twitter stuff

--- WebApp/WebApp/Controllers/TwitterAPIController.cs
+++ WebApp/WebApp/Controllers/TwitterAPIController.cs
@@ -14,23 +14,26 @@ namespace WebApp.Controllers
             public static string _ConsumerSecret = "7nAfcs3IWSf8zbZhP7f5D61RYIhvPljg9bhwyIPQUFhZlYp2nD";
             public static string _AccessToken = "1458372727140671495-s0nFlhJoqJ6MGYoQPxC2p7YZ1PF0fG";
             public static string _AccessTokenSecret = "YguTTU40A6KXoWfH0AHDY7k4gXItPM8po5ANPeymCIy2i";
-            
-            protected TwitterSearchResult GetTwitterSearchResult()
-            {
+
+        protected TwitterSearchResult GetTwitterSearchResult()
+        {
 
             var tweets_search = new SearchOptions { Q = "#stocks", Resulttype = TwitterSearchResultType.Popular };
             TwitterService twService = new TwitterService(_ConsumerKey, _ConsumerSecret, _AccessToken, _AccessTokenSecret);
+
             TwitterSearchResult searchResult = twService.Search(tweets_search);
-
             return searchResult;
-            }
-
+        }
 
         public IActionResult Index()
         {
             var statuses = GetTwitterSearchResult();
+            if (statuses != null)
+            { 
             List<TwitterStatus> tweetList = statuses.Statuses.ToList();
             return View(tweetList);
+            }
+            return View("TwitterError");
         }
 
     }
