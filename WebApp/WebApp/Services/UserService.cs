053a893a62788684763b16d6baabd9385eb4f0af
Stav <stavi1498@gmail.com>
2021-11-13 23:40:53 +0200

    Update Stock search

--- WebApp/WebApp/Services/UserService.cs
+++ WebApp/WebApp/Services/UserService.cs
@@ -23,25 +23,6 @@ namespace WebApp.Services
             return user;
         }
 
-        public async Task UpdateUserStocks(string username, string stockName, int numOfStocks, double price)
-        {
-            User user = await _context.User.FirstOrDefaultAsync(u => u.Username == username);
-            /*var stock = user.StocksList.FirstOrDefault(s => s.Symbol == stockName);
-            if (stock != null)
-            {
-                //stock.
-            }
-            /*
-            if (s == null)
-            {
-                user.StocksList.Add(stockName);
-            }
-            //user.StocksList
-            //user.Stocks.Append()
-            _context.SaveChanges();
-            */
-        }
-
         public int GetStocksAmount(string username, string symbol)
         {
             User user = _context.User.FirstOrDefault(u => u.Username == username);
